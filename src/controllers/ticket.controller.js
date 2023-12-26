1a17a8d3964daa5d58982cc16f05664e4db74a97
Ezequiel Veron <ezequiel.veron@nearest-it.com>
2023-12-26 13:56:53 -0300

    Versión 2.0.2

--- src/controllers/ticket.controller.js
+++ src/controllers/ticket.controller.js
@@ -6,14 +6,18 @@ const ticketController = async (req, res) => {
     const ticketData = await ticketManager.loadTicket(tid);
     if (!ticketData.mail_enviado) {
         const asunto = "Muchas gracias por tu Compra!";
+        if (ticketData.products) {
         let cuerpo = "<html><body><h1>Gracias por tu compra!</h1><p>Estos son los productos que compraste:</p><ul>";
         ticketData.products.forEach(product => {
             cuerpo += "<li>" + product.product.title + " - " + product.quantity + " unidades</li>";
         });
+    }
+        if (ticketData.notAvailableProducts) {
         cuerpo += "</ul><p>Estos son los productos que no pudimos enviarte:</p><ul>";
         ticketData.notAvailableProducts.forEach(product => {
             cuerpo += "<li>" + product.product.title + " - " + product.quantity + " unidades</li>";
         });
+    }
         cuerpo += "</ul><p>El total de tu compra es: $" + ticketData.amount + "</p></body></html>";
         try {
         fetch ('http://localhost:' + config.port + '/mail/send', {
